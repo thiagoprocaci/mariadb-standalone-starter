09e0556ed270d7b3e9cad585cfe82f62d678b387
thiago <thiagoprocaci@gmail.com>
2019-11-09 18:02:54 -0300

    Adicionando log no start

--- src/main/java/br/tbp/Application.java
+++ src/main/java/br/tbp/Application.java
@@ -12,10 +12,14 @@ public class Application {
 
         String userHome = System.getProperty("user.home");
         userHome = userHome + File.separator + "mariadb";
+        int port = 3307;
         DBConfigurationBuilder configBuilder = DBConfigurationBuilder.newBuilder();
-        configBuilder.setPort(3307); // OR, default: setPort(0); => autom. detect free port
+        configBuilder.setPort(port); // OR, default: setPort(0); => autom. detect free port
         configBuilder.setDataDir(userHome); // just an example
         DB db = DB.newEmbeddedDB(configBuilder.build());
+        System.out.println("MariaDB started. Port: " + port + " ; Host: localhost ; user: root ; password: ''" );
         db.start();
+
+
     }
 }
